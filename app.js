a61ee1b4747e984584e32163110d1371be03cae0
Jefferson Lim <Jefferer@gmail.com>
2017-06-17 00:38:42 +0000

    [feature] created initial routes, bootstrap styling

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var hikes = [
+    {name: "Mountain", image: "https://farm4.staticflickr.com/3866/18659273934_9dd488d112.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/453/19094215060_de6549b159.jpg"},
+    {name: "Goat Rest", image: "https://farm3.staticflickr.com/2886/34400868465_f4d39d6361.jpg"},
+    {name: "Mountain", image: "https://farm4.staticflickr.com/3866/18659273934_9dd488d112.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/453/19094215060_de6549b159.jpg"},
+    {name: "Goat Rest", image: "https://farm3.staticflickr.com/2886/34400868465_f4d39d6361.jpg"}    
+];
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/hikingspots", function(req, res) {
-    var hikes = [
-        {name: "Mountain", image: "https://c1.staticflickr.com/3/2770/4466093934_52d2470d81_b.jpg"},
-        {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTttgJpfzusljb4odv33z9mgA39SSC-CghOBWMVzkQssObqGX9"},
-        {name: "Goat Rest", image: "https://s-media-cache-ak0.pinimg.com/736x/8a/98/99/8a9899fe167e6c7525b51a5420d7b3f2.jpg"}
-    ];
-    
     res.render("hikes", {hikes: hikes});
-    
-    
 });
 
+
+app.post("/hikingspots", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newHike = {name: name, image: image};
+    
+    hikes.push(newHike);
+    res.redirect("/hikingspots");
+});
+
+app.get("/hikingspots/new", function(req, res) {
+    res.render("new.ejs");
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Explore Hikes has started!");
