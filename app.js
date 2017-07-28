06d0084e491cbf5d578effbae15eb393df90b406
Jefferson Lim <Jefferer@gmail.com>
2017-07-28 00:30:45 +0000

    [feature] added passport for authentication, added login and register routes

--- app.js
+++ app.js
@@ -2,24 +2,45 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var Hikes = require("./models/hikes");
 var Comments = require("./models/comment");
+var User = require("./models/user");
 var seedDB = require("./seeds");
 
 seedDB();
 mongoose.connect("mongodb://localhost/explore_hikes");
 
-
-
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+
+//passport config
+
+app.use(require("express-session")({
+    secret: "Inspired LBC is up and coming",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//index page- shows all hikes
 app.get("/hikingspots", function(req, res) {
     //get all hikes from db
     Hikes.find({}, function(err, allHikes) {
@@ -71,7 +92,7 @@ app.get("/hikingspots/:id", function(req, res) {
 
 
 //***COMMENTS Routes
-app.get("/hikingspots/:id/comments/new", function(req, res) {
+app.get("/hikingspots/:id/comments/new", isLoggedIn, function(req, res) {
     Hikes.findById(req.params.id, function(err, hike) {
         if (err) {
             console.log(err);
@@ -81,7 +102,7 @@ app.get("/hikingspots/:id/comments/new", function(req, res) {
     });
 })
 
-app.post("/hikingspots/:id/comments", function(req, res) {
+app.post("/hikingspots/:id/comments", isLoggedIn, function(req, res) {
     //find hike by id
     Hikes.findById(req.params.id, function(err, hike) {
         if (err) {
@@ -103,6 +124,55 @@ app.post("/hikingspots/:id/comments", function(req, res) {
     //connect new comment to hike
     //redirect back to show page
 })
+
+
+//AUTH routes******
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+})
+
+//handle sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/hikingspots");
+        })
+    });
+});
+
+//show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+})
+
+//handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/hikingspots",
+        failtureRedirect: "/login"
+    }), function(req, res) {
+});
+
+//logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/hikingspots");
+});
+
+//user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Explore Hikes has started!");
